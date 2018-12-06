0e82237a5f65f9e6f65a218e376366d88c91c831
Achala <achala@aicumen.com>
2018-12-06 17:29:36 +0530

    Nim contract using Emscripten

---
+++ leypa_nim_packagespkg.nim
@@ -0,0 +1,9 @@
+import macros
+
+proc getWelcomeMessage*(): string = "Hello, World!"
+macro exportwasm*(p: untyped): untyped =
+    expectKind(p, nnkProcDef)
+    result = p
+    result.addPragma(newIdentNode("exportc"))
+    result.addPragma(newColonExpr(newIdentNode("codegenDecl"), newLit("__attribute__ ((visibility (\"default\"))) $# $#$#")))
+  
